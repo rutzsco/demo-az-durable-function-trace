791fd27796a17403ea277c27dbcb91cb290689b4
Scott Rutz <scrutz@microsoft.com>
2020-07-28 07:43:13 -0500

    Checkpoint

---
+++ demo-az-durable-functions/TriggerFunction.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace demo_az_durable_functions
+{
+    public static class TriggerFunction
+    {
+        [FunctionName("Function1_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+                                                                [DurableClient] IDurableOrchestrationClient starter,
+                                                                ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("WorkflowFunction", null);
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
---
+++ demo-az-durable-functions/WorkflowFunction.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace demo_az_durable_functions
+{
+    public static class WorkflowFunction
+    {
+        [FunctionName("WorkflowFunction")]
+        public static async Task<List<string>> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("HelloActivity", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("HelloActivity", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("HelloActivity", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("HelloActivity")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
+        }
+    }
+}
