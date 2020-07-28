1ea9ad25dbadabc1b548f2f9665d5340e0c0f9e2
Scott Rutz <scrutz@microsoft.com>
2020-07-28 10:49:58 -0500

    cp

--- demo-az-durable-functions/TriggerFunction.cs
+++ demo-az-durable-functions/TriggerFunction.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
+
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -10,19 +13,30 @@ using Microsoft.Extensions.Logging;
 
 namespace demo_az_durable_functions
 {
-    public static class TriggerFunction
+    public class TriggerFunction
     {
+        private readonly TelemetryClient telemetryClient;
+
+        /// Using dependency injection will guarantee that you use the same configuration for telemetry collected automatically and manually.
+        public TriggerFunction(TelemetryConfiguration telemetryConfiguration)
+        {
+            this.telemetryClient = new TelemetryClient(telemetryConfiguration);
+        }
+
         [FunctionName("Function1_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+        public async Task<HttpResponseMessage> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
                                                                 [DurableClient] IDurableOrchestrationClient starter,
                                                                 ILogger log)
         {
             // Function input comes from the request content.
 
             Activity.Current.AddTag("CUSTOM-PROPERTY", "MY VALUE");
+            telemetryClient.Context.GlobalProperties.Add("CUSTOM-PROPERTY-TC1", "MY VALUE");
 
             string instanceId = await starter.StartNewAsync("WorkflowFunction", null);
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            telemetryClient.TrackTrace($"Started orchestration with ID = '{instanceId}'.");
+
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
     }
